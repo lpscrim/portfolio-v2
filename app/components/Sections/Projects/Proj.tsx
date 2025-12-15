8b4e68aff2f0959ccaa97ece62c3aa513db54d4f
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-12-15 15:42:27 +0000

    Update project descriptions in Proj component and add brief field to Card component for enhanced project details

--- app/components/Sections/Projects/Proj.tsx
+++ app/components/Sections/Projects/Proj.tsx
@@ -14,8 +14,8 @@ const projects = [
   {
     title: "Shopify kiosk QR",
     brief:
-      "This project involved creating a simple, user-friendly kiosk interface for a Shopify store using Next.js. The goal was to enhance the in-store shopping experience by allowing customers to easily browse products and make purchases via QR codes.",
-    description: "",
+      "A custom kiosk QR code system for retail stores built using Shopify's liquid templating language.",
+    description: `Using Shopify's liquid templating language, I created a custom kiosk QR code system for multiple retail stores across various themes. The system allows customers to scan QR codes placed around the store to quickly access product information and make purchases directly from their mobile devices. The QR code was generated using an external API and dynamically linked to a rebuilt checkout page. The interface was then integrated with a custom timer that resets the session. Additionally, all customer orders were then tagged with "kiosk" for easy identification. This system enhances the shopping experience by providing a secure and contactless way for customers to explore products and make purchases, whilst providing data to show in-store engagement.`,
     img: "/projects/qr1.svg",
     vid: "/Vids/Qr.mp4",
   },
@@ -23,9 +23,9 @@ const projects = [
   {
     title: "No Grout About It",
     brief:
-      "This is the website for an independent tiling company, No Grout About It. Built with Next.js, it was designed to offer a modern, responsive, and user-friendly experience.",
+      "This project involved redesigning and developing a website for No Grout About It, an independent tiling company. The site was built using Next.js to create a modern, responsive, and user-friendly experience that effectively showcases the company's services and portfolio.",
     description:
-      "The brief of this website is to provide top-notch web design services that combine aesthetics with functionality.",
+      "This is the website for an independent tiling company, No Grout About It. Built with Next.js, it was redesigned to offer a modern, responsive, and user-friendly experience. The website features a clean design that highlights the company's services and portfolio. By using a similar color palette and local images, the site maintains a consistent brand identity. The site includes an integrated CMS for managing the dynamic blog section, allowing the client to easily update content and images without needing extensive technical knowledge.",
     img: "/projects/gro1.svg",
     vid: "/Vids/Gro.mp4",
   },
@@ -35,7 +35,7 @@ const projects = [
     brief:
       "This project involved designing and developing a website for Cioch, a local business. The site was built using Next.js to ensure it was both visually appealing and highly functional, providing users with an excellent browsing experience.",
     description:
-      "This website aims to create stunning, responsive, and user-friendly websites that help businesses establish a strong online presence.",
+      "This is the website for Cioch, a respected outdoor clothing brand. Built with Next.js, it was redesigned to offer a modern, responsive, and user-friendly experience. The design preserves the charm of the original style while incorporating modern principles. By using a similar color palette and local images, the site maintains a consistent brand identity. SEO was dramatically improved, and the site was integrated with a CMS using Sanity, allowing the owners to update gallery images independently. The site is fully responsive, ensuring a great user experience across all devices.",
     img: "/projects/cio1.svg",
     vid: "/Vids/Cio.mp4",
   },
@@ -43,9 +43,9 @@ const projects = [
   {
     title: "Profile v.1",
     brief:
-      "This project involved creating a personal portfolio website using Next.js. The goal was to showcase my skills, projects, and experience in web design and development through a clean, modern, and responsive design.",
+      " A simple and fast portfolio website built using HTML, CSS, and vanilla JavaScript to achieve optimal performance and compliance.",
     description:
-      "A creative approach to a website that focuses on delivering exceptional user experiences through innovative design solutions.",
+      "After using various frameworks for serverside projects and API requests, I decided to return to HTML/vanillaJS to create a simple and fast portfolio website. With no need for a virtual DOM or huge amounts of reusable components, a more simple approach felt more applicable. No vast libraries and dependencies were required, allowing for a more direct HTML led site. As a result the simple website has achieved 100% scores on lighthouse, is lightning fast and extremely compliant. Browsers that do not support Javascript will still be able to view the full site.",
     img: "/projects/pro1.svg",
     vid: "/Vids/Pro.mp4",
   },
@@ -53,9 +53,9 @@ const projects = [
   {
     title: "Lampman",
     brief:
-      "This project involved designing and developing a website for Lampman, a lighting",
+      "A fully integrated e-commerce site for an antiques restoration company specializing in British lamps, built with NextJS and Tailwind CSS.",
     description:
-      "A creative approach to a website that focuses on delivering exceptional user experiences through innovative design solutions.",
+      "Lampman is a fully integrated e-commerce site built for an antiques restoration company specializing in British lamps. The project uses NextJS and Tailwind CSS for modular development, reducing code repetition and optimizing image loading. The site features a shopping cart and Stripe API integration for secure payments and customer requests. Stripe's API was used to manage limited stock, eliminating the need for a separate backend or inventory management system. After each transaction, stock is updated dynamically and can be managed directly from Stripe's CMS by the client, making the process efficient and cost-effective.",
     img: "/projects/lmp1.svg",
     vid: "/Vids/Lmp.mp4",
   },
@@ -64,7 +64,9 @@ const projects = [
 export default function Proj() {
   return (
     <div className="relative w-full flex flex-col gap-y-50 xl:mr-150 lg:mr-100 md:mr-70 sm:mr-40 mr-15">
-      <h2 className="home-title mt-100 text-background text-5xl lg:text-6xl xl:text-7xl mx-auto font-semibold border-4 border-transparent hover:border-background/80 transition-all duration-1250 rounded-md fade-in pb-1 pt-2 px-2">projects</h2>
+      <h2 className="home-title mt-100 text-background text-5xl lg:text-6xl xl:text-7xl mx-auto font-semibold border-4 border-transparent hover:border-background/80 transition-all duration-1250 rounded-md fade-in pb-1 pt-2 px-2">
+        projects
+      </h2>
 
       <div className="max-w-4xl mt-200 mx-auto text-foreground flex flex-col gap-y-60 lg:gap-y-70 ">
         {projects.map((project, index) => (
--- app/components/UI/Card.tsx
+++ app/components/UI/Card.tsx
@@ -1,39 +1,70 @@
 import Link from "next/link";
 
-export default function Card({ content, main }: { content: { title: string; description: string; img: string; vid: string }; main?: boolean }) {
-    return (
-        <div className="group w-full h-full overflow-hidden rounded-sm hover:shadow-md cursor-pointer justify-center flex flex-col relative transition-all duration-500 ease-in-out">
-            <Link href={"#123"}  >
-            {/* Background image */}
-            <div
-                className="absolute inset-0 w-full h-full bg-cover bg-center"
-                style={{ backgroundImage: `url(${content.img})` }}
-            />
-            <div className={`absolute inset-0 w-full h-full ` 
-                + (main ? "bg-background/65 backdrop-blur" : " bg-foreground/65 backdrop-blur")
-            } />
-            
-            {/* Content */}
-            <div className="px-4 py-5 md:px-8 md:py-7 xl:p-14 relative items-center justify-center flex z-10 ">
-                <video className="w-full h-auto object-cover rounded-sm aspect-video max-w-4xl "
-                src={content.vid}
-                autoPlay
-                loop
-                muted
-                playsInline
-                />
-            </div>
-            <div className={`px-4 py-2 sm:px-6`} style={{ position: "relative", zIndex: 10 }}>
-                <div className="text-base -mt-5 w-full justify-center flex">
-                    <div className="md:text-xl inline-block rounded-xs group-hover:bg-background transition-all duration-700">
-                        <h2 className={`group-hover:bg-foreground/5 px-4 lowercase home-title tracking-wide pt-0.5 transition-all duration-700`
-                            + (main ? " text-foreground" : " text-background")
-                        }>{content.title}</h2>
-                    </div>
-                </div>
-                <p className={`mt-2 text-sm px-2 ` + (main ? "text-foreground/80" : "text-background/80")}>{content.description}</p>
-            </div>
-            </Link>
+export default function Card({
+  content,
+  main,
+}: {
+  content: {
+    title: string;
+    description: string;
+    img: string;
+    vid: string;
+    brief: string;
+  };
+  main?: boolean;
+}) {
+  return (
+    <div className="group w-full h-full overflow-hidden rounded-sm hover:shadow-md cursor-pointer justify-center flex flex-col relative transition-all duration-500 ease-in-out">
+      <Link href={"#123"}>
+        {/* Background image */}
+        <div
+          className="absolute inset-0 w-full h-full bg-cover bg-center"
+          style={{ backgroundImage: `url(${content.img})` }}
+        />
+        <div
+          className={
+            `absolute inset-0 w-full h-full ` +
+            (main
+              ? "bg-background/65 backdrop-blur"
+              : " bg-foreground/65 backdrop-blur")
+          }
+        />
+
+        {/* Content */}
+        <div className="px-4 py-5 md:px-8 md:py-7 xl:p-14 relative items-center justify-center flex z-10 ">
+          <video
+            className="w-full h-auto object-cover rounded-sm aspect-video max-w-4xl "
+            src={content.vid}
+            autoPlay
+            loop
+            muted
+            playsInline
+          />
         </div>
-    );
+        <div
+          className={`px-4 py-2 sm:px-6`}
+          style={{ position: "relative", zIndex: 10 }}
+        >
+          <div className="text-base -mt-5 w-full justify-center flex">
+            <div className="md:text-xl inline-block rounded-xs group-hover:bg-background transition-all duration-700">
+              <h2
+                className={
+                  `group-hover:bg-foreground/5 px-4 lowercase home-title tracking-wide pt-0.5 transition-all duration-700` +
+                  (main ? " text-foreground" : " text-background")
+                }
+              >
+                {content.title}
+              </h2>
+            </div>
+          </div>
+        </div>
+        <div className={`absolute inset-0 z-99 w-full h-full ` + (main ? "hidden" : "")}>
+          <p
+            className={ ` pt-4 text-sm px-4 bg-foreground/20 w-full h-full `}>
+            {content.brief}
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
 }
