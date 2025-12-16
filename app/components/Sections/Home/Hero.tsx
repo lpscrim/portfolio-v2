089a350a7713ed6b005363b2108ee74753faa591
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-12-16 11:37:57 +0000

    fix: update parallax layer heights to use responsive utility classes for improved layout

--- app/components/Sections/Home/Hero.tsx
+++ app/components/Sections/Home/Hero.tsx
@@ -111,8 +111,8 @@ export default function Hero() {
         >
           <div className="parallax-layer pointer-events-none fixed h-[150vh] bg-[url('/Layered/LayeredL1.png')]"></div>
 
-          <div className="parallax-layer pointer-events-none h-full fixed bg-[url('/Layered/LayeredL8.png')] mt-0.5 z-5"></div>
-          <div className="parallax-layer pointer-events-auto fixed top-0 left-0 w-full h-full flex items-center justify-center z-7">
+          <div className="parallax-layer pointer-events-none h-svh fixed bg-[url('/Layered/LayeredL8.png')] mt-0.5 z-5"></div>
+          <div className="parallax-layer pointer-events-auto fixed top-0 left-0 w-full h-svh flex items-center justify-center z-7">
             {/* Desktop Title */}
             <h2
               className="home-title -mt-22 xl:-mt-28 text-background text-5xl lg:text-6xl xl:text-7xl border-4 border-transparent hover:border-background/80 transition-all duration-1250 rounded-md fade-in pb-1 pt-2 px-2 cursor-pointer"
@@ -122,8 +122,8 @@ export default function Hero() {
             </h2>
           </div>
 
-          <div className="parallax-layer pointer-events-none h-full fixed bg-[url('/Layered/LayeredL3.png')] flex items-center justify-center z-10"></div>
-          <div className="parallax-layer pointer-events-none fixed top-0 left-0 w-full h-full flex items-center justify-center z-15">
+          <div className="parallax-layer pointer-events-none h-svh fixed bg-[url('/Layered/LayeredL3.png')] flex items-center justify-center z-10"></div>
+          <div className="parallax-layer pointer-events-none fixed top-0 left-0 w-full h-svh flex items-center justify-center z-15">
             <h2 className="home-title pt-[110vh] text-background/80 text-2xl"></h2>
           </div>
           <div className="parallax-layer pointer-events-none fixed z-15 w-full left-0 top-0 -mt-0.5">
--- app/components/Sections/Projects/ProjectContainer.tsx
+++ app/components/Sections/Projects/ProjectContainer.tsx
@@ -43,14 +43,14 @@ export default function ProjectContainer() {
       >
         <div className="parallax-layer pointer-events-none fixed h-[150vh] bg-[url('/Layered/LayeredL1.png')]"></div>
 
-        <div className="parallax-layer pointer-events-none h-full fixed bg-[url('/Layered/LayeredL8.png')] mt-0.5 z-5"></div>
-        <div className="parallax-layer pointer-events-auto fixed top-0 left-0 w-full h-full flex flex-col items-center justify-start z-7">
+        <div className="parallax-layer pointer-events-none h-lvh fixed bg-[url('/Layered/LayeredL8.png')] mt-0.5 z-5"></div>
+        <div className="parallax-layer pointer-events-auto fixed top-0 left-0 w-full h-lvh flex flex-col items-center justify-start z-7">
           <h2 className="xl:ml-200 sm:ml-100 ml-70  home-title mt-150 text-background text-5xl lg:text-6xl xl:text-7xl  border-4 border-transparent hover:border-background/80 transition-all duration-1250 rounded-md fade-in pb-1 pt-2 px-2">2025</h2>
           <h2 className="xl:ml-200 sm:ml-100 ml-70 home-title mt-140 sm:mt-190 text-background text-5xl lg:text-6xl xl:text-7xl  border-4 border-transparent hover:border-background/80 transition-all duration-1250 rounded-md fade-in pb-1 pt-2 px-2">2024</h2>
           <h2 className="xl:ml-200 sm:ml-100 ml-70 home-title mt-80 text-background text-5xl lg:text-6xl xl:text-7xl  border-4 border-transparent hover:border-background/80 transition-all duration-1250 rounded-md fade-in pb-1 pt-2 px-2">2023</h2>        
         </div>
 
-        <div className="parallax-layer pointer-events-none h-full fixed bg-[url('/Layered/LayeredL3.png')] flex items-center justify-center z-10"></div>
+        <div className="parallax-layer pointer-events-none h-lvh fixed bg-[url('/Layered/LayeredL3.png')] flex items-center justify-center z-10"></div>
         <div className="parallax-layer pointer-events-auto fixed top-0 left-0 w-full h-4vh flex items-center justify-center z-15 px-1 sm:px-4">
           <Proj />
         </div>
