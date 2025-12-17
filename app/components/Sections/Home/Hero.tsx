20d912e3c26f5da66f58d8e24244aaadee3d331b
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-12-17 18:49:30 +0000

    Add LayeredL8.webp image and create debug log file

--- app/components/Sections/Home/Hero.tsx
+++ app/components/Sections/Home/Hero.tsx
@@ -41,9 +41,9 @@ export default function Hero() {
           id="parallax-container"
           className="flex parallax-container relative overflow-x-hidden"
         >
-          <div className="parallax-layer pointer-events-none fixed h-[150vh] bg-[url('/Layered/LayeredL1.png')]"></div>
+          <div className="parallax-layer pointer-events-none fixed h-[150vh] bg-[url('/Layered/LayeredL1.webp')]"></div>
 
-          <div className="parallax-layer pointer-events-none h-svh fixed bg-[url('/Layered/LayeredL8.png')] mt-0.5 z-5"></div>
+          <div className="parallax-layer pointer-events-none h-svh fixed bg-[url('/Layered/LayeredL8.webp')] mt-0.5 z-5"></div>
           <div className="parallax-layer pointer-events-auto fixed top-0 left-0 w-full h-svh flex items-center justify-center z-7">
             {/* Desktop Title */}
             <h2
@@ -54,12 +54,12 @@ export default function Hero() {
             </h2>
           </div>
 
-          <div className="parallax-layer pointer-events-none h-svh fixed bg-[url('/Layered/LayeredL3.png')] flex items-center justify-center z-10"></div>
+          <div className="parallax-layer pointer-events-none h-svh fixed bg-[url('/Layered/LayeredL3.webp')] flex items-center justify-center z-10"></div>
           <div className="parallax-layer pointer-events-none fixed top-0 left-0 w-full h-svh flex items-center justify-center z-15">
             <h2 className="home-title pt-[110vh] text-background/80 text-2xl"></h2>
           </div>
           <div className="parallax-layer pointer-events-none fixed z-15 w-full left-0 top-0 -mt-0.5">
-            <div className="w-full h-svh bg-[url('/Layered/LayeredL4.png')] bg-cover bg-center"></div>
+            <div className="w-full h-svh bg-[url('/Layered/LayeredL4.webp')] bg-cover bg-center"></div>
             <div className="w-full h-[50vh] bg-foreground"></div>
           </div>
         </div>
