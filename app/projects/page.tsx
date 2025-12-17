74aa56642b417da91d7be77493252436a595a5a2
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2025-12-17 19:20:30 +0000

    fix: adjust minimum height values for project section for better layout consistency

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,7 +5,7 @@ import Footer from "../components/Layout/Footer";
 export default function ProjectsMain() {
     return (
         <section id="projects" className="">
-            <div className="relative w-full min-h-[500lvh] sm:min-h-[520lvh] md:min-h-[640lvh] lg:min-h-[680lvh] xl:min-h-[700lvh] ">
+            <div className="relative w-full min-h-[520lvh]  md:min-h-[550lvh] lg:min-h-[620lvh] xl:min-h-[700lvh] ">
                 <ProjectContainer />
             </div>
             <section id="trigger"></section>
