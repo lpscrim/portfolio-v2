dee61c8b1a5f7e2e0e659a31bf5f70ddbe8af850
Lewis Scrimgeour <112709088+lpscrim@users.noreply.github.com>
2026-01-10 19:45:04 +0000

    fix: reorder services in the list and adjust layout for improved presentation

--- app/components/Sections/Home/Services.tsx
+++ app/components/Sections/Home/Services.tsx
@@ -7,28 +7,31 @@ export default function Services() {
     "SEO",
     "Brand Identity",
     "Shopify",
-    "UI/UX",
-    "Responsive Design",
     "Accessibility",
+    "Responsive Design",
+    "UI/UX",
   ];
 
   return (
     <section id="services" className="w-full relative">
-        <div className="pb-2 lg:px-16">
-      <div className="max-w-4xl mx-auto text-center sm:pl-0">
-        {/* Services */}
-        <h2 className="py-4 sm:py-8 text-3xl sm:text-4xl">Services</h2>
-        
-        <div className="py-10 border-t border-b border-foreground/80 mt-8 mb-14 ">
-          <div className="grid grid-cols-2 sm:grid-cols-3 italic gap-y-2 gap-x-4 text-lg sm:text-xl xl:text-2xl justify-center">
-            {services.map((service, i) => (
-              <p key={i} className="text-center">
-                {service}
-              </p>
-            ))}
+      <div className="pb-2 lg:px-16">
+        <div className="max-w-4xl mx-auto text-center sm:pl-0">
+          {/* Services */}
+          <h2 className="py-4 sm:py-8 text-3xl sm:text-4xl">Services</h2>
+
+          <div className="py-10 border-t border-b border-foreground/80 mt-8 mb-14 ">
+            <div className="flex flex-wrap justify-center italic gap-y-2 gap-x-4 text-lg sm:text-xl xl:text-2xl">
+              {services.map((service, i) => (
+                <p
+                  key={i}
+                  className="text-center w-1/3 sm:w-1/3 lg:w-1/4 box-border px-2"
+                >
+                  {service}
+                </p>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
       </div>
     </section>
   );
